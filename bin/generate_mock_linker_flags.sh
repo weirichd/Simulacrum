5ba975a89e11075c54819c9d2cc2c65fa4b11e21
Zack Brown <zbrown@pillartechnology.com>
2017-02-14 11:58:44 -0500

    fix generate mock linker flags

--- bin/generate_mock_linker_flags.sh
+++ bin/generate_mock_linker_flags.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-grep "^TEST_DOUBLE(" $1  | \
+grep "^TEST_DOUBLE(" $1 | \
     awk -F"(" '{print $2}' | \
-    awk -F"," '{print $3}' | \
     sed 's/^[ \t]*/--wrap=/' | \
     tr "\n" ", " |\
-    sed 's/,$/ /;s/^/-Wl,/'
+    sed 's/,$/ /;s/)//g;s/^/-Wl,/'
